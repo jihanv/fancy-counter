46f81eeb7e8fbaf9a3b3b7f03a1231bd126922a7
jihanv <hedjmny@gmail.com>
2025-07-14 21:14:59 +0900

    Add plus and minus functions

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,14 +2,16 @@ import Title from "./Title";
 import Count from "./Count";
 import ResetButton from "./ResetButton";
 import CountButtons from "./CountButtons";
+import { useState } from "react";
 
 export default function Card() {
+  const [count, setCount] = useState(0);
   return (
     <div className="card">
       <Title />
-      <Count />
+      <Count count={count} />
       <ResetButton />
-      <CountButtons />
+      <CountButtons setCount={setCount} />
     </div>
   );
 }
